6dfa90ebe368deebeb63009f7c581e6f2f2ca121
Yuko Honda <yuko@omase.com>
2012-07-31 20:36:54 +0900

    yappari unicode

--- study/pyfes/items.py
+++ study/pyfes/items.py
@@ -3,9 +3,11 @@ with open('items.csv', 'rb') as f:
   reader = csv.reader(f)
   itemDict = {}
   for row in reader:
-    itemName = row[1]
+    itemName = row[1].decode('cp932')
     if itemDict.has_key(itemName):
       itemDict[itemName] += 1
     else:
       itemDict[itemName] = 1      
+  
   print itemDict
+  

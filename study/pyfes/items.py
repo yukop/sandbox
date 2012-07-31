8950da02b7b82d298c47737256d9589141043305
Yuko Honda <yuko@omase.com>
2012-07-31 20:37:56 +0900

    create def

--- study/pyfes/items.py
+++ study/pyfes/items.py
@@ -1,13 +1,20 @@
-import csv
-with open('items.csv', 'rb') as f:
-  reader = csv.reader(f)
-  itemDict = {}
-  for row in reader:
-    itemName = row[1].decode('cp932')
-    if itemDict.has_key(itemName):
-      itemDict[itemName] += 1
-    else:
-      itemDict[itemName] = 1      
-  
-  print itemDict
-  
+# coding: utf-8
+
+def makeDictFromCsv(filename):
+  import csv
+  with open(filename, 'rb') as f:
+    reader = csv.reader(f)
+    itemDict = {}
+    for row in reader:
+      itemName = row[1].decode('cp932')
+      if itemDict.has_key(itemName):
+        itemDict[itemName] += 1
+      else:
+        itemDict[itemName] = 1
+  return itemDict
+
+def printResult(dict):
+  for k, v in dict.items():
+    print k, str(v) + u'個'
+
+printResult(makeDictFromCsv('items.csv'))

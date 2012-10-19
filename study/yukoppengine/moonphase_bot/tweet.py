26de4ae0a3371a3dca15bbecd1d11272473cb5bd
Yuko Honda <yuko@omase.com>
2012-10-19 18:00:29 +0900

    added cron.yaml

--- study/yukoppengine/moonphase_bot/tweet.py
+++ study/yukoppengine/moonphase_bot/tweet.py
@@ -24,7 +24,70 @@ url = 'https://api.twitter.com/1/statuses/update.json'
 
 a = Astral()
 moonphase = a.moon_phase(datetime.datetime.now(), None)
-params = { 'status': "きょうの月齢は " + str(moonphase) + " だよー！"}
+if moonphase == 1:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。新月だよ。"
+elif moonphase == 2:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。既朔っていうらしい。"
+elif moonphase == 3:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。三日月だね。眉月、蛾眉、繊月ともいうし、たくさんの異名があるよ。crescent もクロワッサンも三日月のことなんだって！"
+elif moonphase == 4:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 5:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 6:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 7:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。上弦の月だね。"
+elif moonphase == 8:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 9:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 10:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。十日夜の月。"
+elif moonphase == 11:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 12:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 13:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。十三夜月、もうすぐ満月だね。"
+elif moonphase == 14:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。小望月！"
+elif moonphase == 15:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。満月だよ。"
+elif moonphase == 16:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。十六夜。不知夜月ともいうんだって。"
+elif moonphase == 17:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。立待月なのでがんばって待とう。"
+elif moonphase == 18:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。居待月。昨日は月の出を立って待ったけど、きょうは座って待つんだね。"
+elif moonphase == 19:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。寝待月だよ。待ちきれなくてついにねちゃったね。"
+elif moonphase == 20:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。更待月は夜更けごろに出てくるらしいよ。"
+elif moonphase == 21:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 22:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 23:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。下弦の月だねえ。"
+elif moonphase == 24:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 25:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 26:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。有明月は夜明けごろに出てくるよ。"
+elif moonphase == 27:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 28:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 29:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。"
+elif moonphase == 30:
+  tweet = "きょうの月齢は " + str(moonphase) + " だよ。三十日月。もうすぐ新月だね。"
+else:
+  tweet = "うーん。計算できなかったみたい。"
+
+params = { 'status': tweet }
 result = client.make_request(url,
                              token = user_token,
                              secret = user_secret,
